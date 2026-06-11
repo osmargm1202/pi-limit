44368b35fd8ee09c77e74d6d902b18f9e9d0eec4
osmar <osmargm1202@gmail.com>
2026-06-11 23:43:54 +0000

    test: rename pi-limit package shape test

--- tests/package-shape.test.mjs
+++ tests/package-shape.test.mjs
@@ -4,7 +4,7 @@ import test from "node:test";
 
 const pkg = JSON.parse(readFileSync(new URL("../package.json", import.meta.url), "utf8"));
 
-test("package is a Pi extension scaffold for orgm limits", () => {
+test("package exposes the orgm limits Pi extension", () => {
   assert.equal(pkg.name, "pi-limit");
   assert.deepEqual(pkg.pi.extensions, ["./extensions/limit.ts"]);
   assert.ok(pkg.description.includes("/orgm-limits"));
